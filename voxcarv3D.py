5b360c2873b4a42c85c7eaef561e45c4da1f132b
Abdelmouttaleb Dakri <dakria@ensipc161.ensimag.fr>
2022-01-26 16:36:26 +0100

    local changes

--- voxcarv3D.py
+++ voxcarv3D.py
@@ -38,7 +38,7 @@ calib = np.array([
 
 # Build 3D grids
 # 3D Grids are of size: resolution x resolution x resolution/2
-resolution = 60
+resolution = 200
 step = 2 / resolution
 
 # Voxel coordinates
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     print(X.shape)
     i = 1
     # read the input silhouettes
-    for g in range(1):
+    for g in range(12):
         myFile = "image{0}.pgm".format(g)
         print(myFile)
         img = mpimg.imread(myFile)
